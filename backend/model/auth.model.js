b74cad931fceec94110f22c0182f4e6cd0202ed7
SGMohan <iamsgmohan@gmail.com>
2025-07-18 23:00:02 +0530

    Completed

--- backend/model/auth.model.js
+++ backend/model/auth.model.js
@@ -1,37 +1,43 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const userSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Please provide a username"],
-    unique: true,
-    trim: true,
-    maxlength: [20, "Name cannot be more than 20 characters"],
-  },
-  email: {
-    type: String,
-    required: [true, "Please provide an email"],
-    unique: true,
-    match: [
-      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-      "Please provide a valid email",
-    ],
-  },
-  password: {
-    type: String,
-    required: [true, "Please provide a password"],
-    minlength: [12, "Password must be at least 6 characters"],
-    select: false,
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please provide a username"],
+      unique: true,
+      trim: true,
+      maxlength: [20, "Name cannot be more than 20 characters"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please provide an email"],
+      unique: true,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please provide a valid email",
+      ],
+    },
+    password: {
+      type: String,
+      required: [true, "Please provide a password"],
+      minlength: [12, "Password must be at least 6 characters"],
+      select: false,
     },
     resetToken: {
-        type: String,
-        default: null,
+      type: String,
+      default: null,
     },
     resetTokenExpiry: {
-        type: Date,
-        default: null,
+      type: Date,
+      default: null,
     },
-},{timestamps: true});
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const UserModel = mongoose.model("users", userSchema);
+// console.log("User Model Created Successfully", UserModel);
 module.exports = UserModel;
